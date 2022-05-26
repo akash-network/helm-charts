64023528cc5cd38aac862b454adeb79ed79f58e0
Andrey Arapov <andrey.arapov@nixaid.com>
2022-05-26 17:29:10 +0000

    SUPPORT-178 provider: support persistent storage

--- charts/akash-provider/scripts/price_script_chia.sh
+++ charts/akash-provider/scripts/price_script_chia.sh
@@ -9,7 +9,10 @@ data_in=$(jq .)
 
 cpu_requested=$(echo "$data_in" | jq -r '(map(.cpu * .count) | add) / 1000')
 memory_requested=$(echo "$data_in" | jq -r '(map(.memory * .count) | add) / pow(1024; 3)')
-ephemeral_storage_requested=$(echo "$data_in" | jq -r '([.[].storage[].size] | add) / pow(1024; 3)')
+ephemeral_storage_requested=$(echo "$data_in" | jq -r '([.[].storage[] | select(.class == "ephemeral").size // 0] | add) / pow(1024; 3)')
+hdd_pers_storage_requested=$(echo "$data_in" | jq -r '([.[].storage[] | select(.class == "beta1").size // 0] | add) / pow(1024; 3)')
+ssd_pers_storage_requested=$(echo "$data_in" | jq -r '([.[].storage[] | select(.class == "beta2").size // 0] | add) / pow(1024; 3)')
+nvme_pers_storage_requested=$(echo "$data_in" | jq -r '([.[].storage[] | select(.class == "beta3").size // 0] | add) / pow(1024; 3)')
 
 # cache AKT price for 60 minutes to reduce the API pressure as well as to slightly accelerate the bidding (+5s)
 CACHE_FILE=/tmp/aktprice.cache
@@ -49,7 +52,10 @@ set +e
 
 #Price in USD
 TARGET_MEMORY="1.25"
-TARGET_HD="0.25"
+TARGET_HD_EPHEMERAL="0.25" # previously TARGET_HD
+TARGET_HD_PERS_HDD="0.30"  # beta1
+TARGET_HD_PERS_SSD="0.40"  # beta2
+TARGET_HD_PERS_NVME="0.45" # beta3
 TARGET_CPU="4.50"
 
 #Chia Madmax Plotter
@@ -87,7 +93,7 @@ elif (( $(echo "$memory_requested >= $chia_madmax_memory" | bc -l) && \
   total_cost_usd_target=$(bc -l <<<"($cpu_requested * $TARGET_CPU)")
 else
   #Normal deployment
-  total_cost_usd_target=$(bc -l <<<"(($cpu_requested * $TARGET_CPU) + ($memory_requested * $TARGET_MEMORY) + ($ephemeral_storage_requested * $TARGET_HD))")
+  total_cost_usd_target=$(bc -l <<<"(($cpu_requested * $TARGET_CPU) + ($memory_requested * $TARGET_MEMORY) + ($ephemeral_storage_requested * $TARGET_HD_EPHEMERAL) + ($hdd_pers_storage_requested * $TARGET_HD_PERS_HDD) + ($ssd_pers_storage_requested * $TARGET_HD_PERS_SSD) + ($nvme_pers_storage_requested * $TARGET_HD_PERS_NVME))")
 fi
 
 total_cost_akt_target=$(bc -l <<<"(${total_cost_usd_target}/$usd_per_akt)")
