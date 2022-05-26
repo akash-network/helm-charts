27e1efd04e1841622446b4942f2014b5aa966a78
Andrey Arapov <andrey.arapov@nixaid.com>
2022-05-26 17:01:50 +0000

    SUPPORT-178 provider: better variable names in chia script

--- charts/akash-provider/scripts/price_script_chia.sh
+++ charts/akash-provider/scripts/price_script_chia.sh
@@ -7,14 +7,9 @@
 
 data_in=$(jq .)
 
-cpu_total=$(echo "$data_in" | jq 'map(.cpu * .count) | add')
-memory_total=$(echo "$data_in" | jq 'map(.memory * .count) | add')
-storage_total=$(echo "$data_in" | jq -r '[.[].storage[].size] | add')
-
-cpu_total_threads=$(echo $cpu_total | awk '{print $1/1000}')
-memory_gb=$(echo $memory_total | awk '{print $1/1024/1024/1024}')
-hd_gb=$(echo $storage_total | awk '{print $1/1024/1024/1024}')
-
+cpu_requested=$(echo "$data_in" | jq 'map(.cpu * .count) | add' | awk '{print $1/1000}')
+memory_requested=$(echo "$data_in" | jq 'map(.memory * .count) | add' | awk '{print $1/1024/1024/1024}')
+ephemeral_storage_requested=$(echo "$data_in" | jq -r '[.[].storage[].size] | add' | awk '{print $1/1024/1024/1024}')
 
 # cache AKT price for 60 minutes to reduce the API pressure as well as to slightly accelerate the bidding (+5s)
 CACHE_FILE=/tmp/aktprice.cache
@@ -79,20 +74,20 @@ chia_bladebit_memory_max=512
 chia_bladebit_storage=715
 chia_bladebit_storage_max=3200
 
-if (( $(echo "$memory_gb >= $chia_bladebit_memory" | bc -l) && \
-      $(echo "$cpu_total_threads >= $chia_bladebit_cpu" | bc -l) )); then
+if (( $(echo "$memory_requested >= $chia_bladebit_memory" | bc -l) && \
+      $(echo "$cpu_requested >= $chia_bladebit_cpu" | bc -l) )); then
   #Bladebit detected
   TARGET_CPU="20"
-  total_cost_usd_target=$(bc -l <<<"($cpu_total_threads * $TARGET_CPU)")
-elif (( $(echo "$memory_gb >= $chia_madmax_memory" | bc -l) && \
-        $(echo "$cpu_total_threads >= $chia_madmax_cpu" | bc -l) && \
-        $(echo "$cpu_total_threads <= $chia_madmax_cpu_max" | bc -l) )); then
+  total_cost_usd_target=$(bc -l <<<"($cpu_requested * $TARGET_CPU)")
+elif (( $(echo "$memory_requested >= $chia_madmax_memory" | bc -l) && \
+        $(echo "$cpu_requested >= $chia_madmax_cpu" | bc -l) && \
+        $(echo "$cpu_requested <= $chia_madmax_cpu_max" | bc -l) )); then
   #Madmax detected
   TARGET_CPU="15"
-  total_cost_usd_target=$(bc -l <<<"($cpu_total_threads * $TARGET_CPU)")
+  total_cost_usd_target=$(bc -l <<<"($cpu_requested * $TARGET_CPU)")
 else
   #Normal deployment
-  total_cost_usd_target=$(bc -l <<<"(($cpu_total_threads * $TARGET_CPU) + ($memory_gb * $TARGET_MEMORY) + ($hd_gb * $TARGET_HD))")
+  total_cost_usd_target=$(bc -l <<<"(($cpu_requested * $TARGET_CPU) + ($memory_requested * $TARGET_MEMORY) + ($ephemeral_storage_requested * $TARGET_HD))")
 fi
 
 total_cost_akt_target=$(bc -l <<<"(${total_cost_usd_target}/$usd_per_akt)")
