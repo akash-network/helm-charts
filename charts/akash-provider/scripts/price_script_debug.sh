bb90d21b6397128267b1984f7dd5cdae99648ed0
Andrey Arapov <andrey.arapov@nixaid.com>
2022-05-27 23:13:22 +0000

    SUPPORT-180 provider: add price debug script

---
+++ charts/akash-provider/scripts/price_script_debug.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# for debugging the bid price script.
+
+echo "====================== start ======================" >> /tmp/log
+cat - >> /tmp/log
+
+echo "====================== end ======================" >> /tmp/log
+echo 1
