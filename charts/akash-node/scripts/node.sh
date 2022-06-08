07f64a11801c466f8925d1edd18f3edd084108fb
Andrey Arapov <andrey.arapov@nixaid.com>
2022-06-08 12:43:42 +0000

    ENG-375 akash-node: remove nonsense
    AKASH_P2P_PRIVATE_PEER_IDS is comma separated list of peer IDs to keep private (will not be gossiped to othe
    r peers) which does not make any sense as we aren't using private peers
    nor does it make sense to nmap through whole list of (25 as of now)
    peers to just pick a single working one. It just slows down the
    bootstrap.
    
    and we already use akash_node.peers, which go to AKASH_P2P_PERSISTENT_PEERS and should be more than enough.

--- charts/akash-node/scripts/node.sh
+++ charts/akash-node/scripts/node.sh
@@ -11,27 +11,12 @@ curl -s "$AKASH_NET/genesis.json" > "$AKASH_HOME/config/genesis.json"
 
 if [[ $AKASH_NET == "https://raw.githubusercontent.com/ovrclk/net/master/mainnet" ]]
 then
-  apt -y install jq aria2 wget nmap > /dev/null 2>&1
+  apt -y install jq aria2 > /dev/null 2>&1
   rm -rf $AKASH_HOME/data ; mkdir -p $AKASH_HOME/data ; cd $AKASH_HOME/data
   SNAPSHOT_URL=$(curl https://cosmos-snapshots.s3.filebase.com/akash/pruned/snapshot.json | jq -r .latest)
   echo "Using latest blockchain snapshot, $SNAPSHOT_URL" ; aria2c --out=snapshot.tar.gz --summary-interval 15 --check-certificate=false --max-tries=99 --retry-wait=5 --always-resume=true --max-file-not-found=99 --conditional-get=true -s 16 -x 16 -k 1M -j 1 $SNAPSHOT_URL
   tar -zxvf snapshot.tar.gz
   rm -f snapshot.tar.gz
-  echo "Get working peers to sync an RPC node with!"
-  wget https://raw.githubusercontent.com/ovrclk/net/master/mainnet/peer-nodes.txt
-  ip=$(cat peer-nodes.txt)
-  for i in $ip; do
-    original=$i ; i=$(echo $i | cut -d@ -f2-)
-    solo_ip=$(echo $i | cut -f1 -d":")
-    port=$(echo $i | cut -d":" -f2-)
-    nmap -p $port $solo_ip | grep "down" &> /dev/null
-    if [ $? == 0 ]; then
-      echo "$solo_ip is unreachable"
-    else
-      echo "$original" >> good-nodes.txt
-    fi
-  done
-  export AKASH_P2P_PRIVATE_PEER_IDS=$(cat good-nodes.txt | shuf -n 1)
 fi
 
 /bin/akash validate-genesis
