ef31e5180e883622de60abc2895e648fc7827d71
88plug <19512127+88plug@users.noreply.github.com>
2022-08-18 15:32:02 -0700

    Update node.sh

--- charts/akash-node/scripts/node.sh
+++ charts/akash-node/scripts/node.sh
@@ -34,7 +34,7 @@ else
       SNAPSHOT_URL=$(curl -s https://polkachu.com/tendermint_snapshots/akash | grep tar.lz4 | head -n1 | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i')
       echo "Using latest Polkachu blockchain snapshot, $SNAPSHOT_URL"
       aria2c --out=snapshot.tar.lz4 --summary-interval 15 --check-certificate=false --max-tries=99 --retry-wait=5 --always-resume=true --max-file-not-found=99 --conditional-get=true -s 16 -x 16 -k 1M -j 1 $SNAPSHOT_URL
-      lz4 -c -d snapshot.tar.lz4 | tar -x -C $AKASH_HOME/data
+      lz4 -c -d snapshot.tar.lz4 | tar -x -C $AKASH_HOME
       rm -f snapshot.tar.gz
     else
       SNAPSHOT_URL=$(curl -s https://cosmos-snapshots.s3.filebase.com/akash/pruned/snapshot.json | jq -r .latest)
