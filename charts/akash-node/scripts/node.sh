3085278daee6b4e3d93fc0feaea950ec5c43b4ec
88plug <19512127+88plug@users.noreply.github.com>
2022-08-18 22:44:22 -0700

    [akash-node] Add support for Polkachu Snapshot + Fix Startup Bug (#100)
    * Update Chart.yaml
    
    * Add SNAPSHOT_POLKACHU
    
    * Add snapshot_polkachu
    
    * Add support for Polkachu snapshot
    
    * Cleanup data folder check
    
    Was blocking snapshot usage
    
    * Update for perfect SNAPSHOT_POLKACHU
    
    * Update api_enable flag
    
    * akash_node.api_enable
    
    * Clean up starting logic
    
    * Update Chart.yaml
    
    * Set snapshot_polkachu default to false
    
    We need to confirm the best method to get the most current snapshot URL - my method is not production ready.
    
    * Update node.sh
    
    * Update Chart.yaml

--- charts/akash-node/scripts/node.sh
+++ charts/akash-node/scripts/node.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -x
 
+#Install utils
+apt update && apt -y --no-install-recommends install ca-certificates curl jq > /dev/null 2>&1
+
+#Check if Home data exists, if not create it.
 if [ ! -d "$AKASH_HOME/data" ]
 then
-  /bin/akash init --chain-id "$AKASH_CHAIN_ID" "$AKASH_MONIKER"
-fi
-
-apt update && apt -y --no-install-recommends install ca-certificates curl jq > /dev/null 2>&1
-curl -s "$AKASH_NET/genesis.json" > "$AKASH_HOME/config/genesis.json"
-
-mkdir -p $AKASH_HOME/data
+/bin/akash init --chain-id "$AKASH_CHAIN_ID" "$AKASH_MONIKER"
 cd $AKASH_HOME/data
-
+curl -s "$AKASH_NET/genesis.json" > "$AKASH_HOME/config/genesis.json"
 if [ "$AKASH_STATESYNC_ENABLE" == true ]; then
   echo "state-sync is enabled, figure the right trust height & derive its hash"
 
@@ -30,16 +28,27 @@ if [ "$AKASH_STATESYNC_ENABLE" == true ]; then
   export AKASH_STATESYNC_TRUST_HASH=$TRUST_HASH
 
 else
-  if [ ! -d "$AKASH_HOME/data" ]
-  then
-    apt -y --no-install-recommends install aria2  > /dev/null 2>&1
-    SNAPSHOT_URL=$(curl https://cosmos-snapshots.s3.filebase.com/akash/pruned/snapshot.json | jq -r .latest)
-    echo "Using latest blockchain snapshot, $SNAPSHOT_URL"
-    aria2c --out=snapshot.tar.gz --summary-interval 15 --check-certificate=false --max-tries=99 --retry-wait=5 --always-resume=true --max-file-not-found=99 --conditional-get=true -s 16 -x 16 -k 1M -j 1 $SNAPSHOT_URL
-    tar -zxvf snapshot.tar.gz
-    rm -f snapshot.tar.gz
-  fi
+  apt -y --no-install-recommends install aria2 > /dev/null 2>&1
+    if [ "$SNAPSHOT_POLKACHU" == true ]; then
+      apt -y --no-install-recommends install lz4 > /dev/null 2>&1
+      SNAPSHOT_URL=$(curl -s https://polkachu.com/tendermint_snapshots/akash | grep tar.lz4 | head -n1 | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i')
+      echo "Using latest Polkachu blockchain snapshot, $SNAPSHOT_URL"
+      aria2c --out=snapshot.tar.lz4 --summary-interval 15 --check-certificate=false --max-tries=99 --retry-wait=5 --always-resume=true --max-file-not-found=99 --conditional-get=true -s 16 -x 16 -k 1M -j 1 $SNAPSHOT_URL
+      lz4 -c -d snapshot.tar.lz4 | tar -x -C $AKASH_HOME
+      rm -f snapshot.tar.gz
+    else
+      SNAPSHOT_URL=$(curl -s https://cosmos-snapshots.s3.filebase.com/akash/pruned/snapshot.json | jq -r .latest)
+      echo "Using latest Cosmos blockchain snapshot, $SNAPSHOT_URL"
+      aria2c --out=snapshot.tar.gz --summary-interval 15 --check-certificate=false --max-tries=99 --retry-wait=5 --always-resume=true --max-file-not-found=99 --conditional-get=true -s 16 -x 16 -k 1M -j 1 $SNAPSHOT_URL
+      tar -zxvf snapshot.tar.gz
+      rm -f snapshot.tar.gz
+    fi
+fi
+/bin/akash start
+else
+  echo "Found Akash data folder!"
+  cd $AKASH_HOME/data
+  /bin/akash start
 fi
 
-/bin/akash start
 if [[ $AKASH_DEBUG == "true" ]]; then sleep 5000; fi
