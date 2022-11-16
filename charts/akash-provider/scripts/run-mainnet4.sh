0b18e83a4f231ee42b3ed7bbed5fff3067ddeb32
Andrey Arapov <107317698+andy108369@users.noreply.github.com>
2022-11-16 18:44:30 +0100

    provider: fix bid script for mainnet4 (#142)

--- charts/akash-provider/scripts/run-mainnet4.sh
+++ charts/akash-provider/scripts/run-mainnet4.sh
@@ -7,7 +7,7 @@ set -x
 ##
 # Import key
 ##
-cat "$AKASH_BOOT_KEYS/key-pass.txt" | { cat ; echo ; } | /bin/akash --home="$AKASH_HOME" keys import --keyring-backend="$AKASH_KEYRING_BACKEND"  "$AKASH_FROM" "$AKASH_BOOT_KEYS/key.txt"
+cat "$AKASH_BOOT_KEYS/key-pass.txt" | { cat ; echo ; } | provider-services --home="$AKASH_HOME" keys import --keyring-backend="$AKASH_KEYRING_BACKEND"  "$AKASH_FROM" "$AKASH_BOOT_KEYS/key.txt"
 
 ##
 # Check the Akash Node is working
@@ -21,7 +21,7 @@ fi
 until [[ $(curl -s $AKASH_NODE/status | jq -r .result.sync_info.catching_up) == "false" ]]; do sleep 15; echo "Akash node not ready. Retrying";  done
 
 # Check Akash RPC node isn't running behind too much and abort if it does.
-DATE_AKASH=$(akash status | jq -r '.SyncInfo.latest_block_time')
+DATE_AKASH=$(curl -s $AKASH_NODE/status | jq -r '.result.sync_info.latest_block_time')
 TS_AKASH=$(date +%s --date "$DATE_AKASH")
 TS=$(date +%s)
 DIFF=$(echo "$TS - $TS_AKASH" | bc)
@@ -55,17 +55,17 @@ owner: {{ .Values.from }}
 EOT
 
 # Figure the provider address in case the user passes `--from=<key_name>` instead of `--from=<akash1...>` address.
-PROVIDER_ADDRESS="$(/bin/akash keys show $AKASH_FROM -a)"
+PROVIDER_ADDRESS="$(provider-services keys show $AKASH_FROM -a)"
 if [[ -z "$PROVIDER_ADDRESS" ]]; then
   echo "PROVIDER_ADDRESS variable is empty. Something went wrong"
   exit 1
 fi
 
-/bin/akash query provider get $PROVIDER_ADDRESS -o json
+provider-services query provider get $PROVIDER_ADDRESS -o json
 if [[ $? -ne 0 ]]; then
   echo "Could not find provider: $PROVIDER_ADDRES on the blockchain when querying Akash RPC node: $AKASH_NODE"
   echo "Attempting to create a new provider ..."
-  /bin/akash tx provider create provider.yaml >deploy.log 2>&1
+  provider-services tx provider create provider.yaml >deploy.log 2>&1
   DEPLOY=$(cat deploy.log)
 fi
 
@@ -82,11 +82,11 @@ elif [[ $DEPLOY == *"Error"* ]]; then
 fi
 
 echo "Checking whether provider.yaml needs to be updated on the chain ..."
-REMOTE_PROVIDER="$(akash query provider get $PROVIDER_ADDRESS -o json | jq | sha1sum | awk '{print $1}')"
-LOCAL_PROVIDER="$(/bin/akash tx provider update provider.yaml --offline --generate-only --from $PROVIDER_ADDRESS | jq -r '.body.messages[]' | jq -r 'del(."@type")' | sha1sum | awk '{print $1}')"
+REMOTE_PROVIDER="$(provider-services query provider get $PROVIDER_ADDRESS -o json | jq | sha1sum | awk '{print $1}')"
+LOCAL_PROVIDER="$(provider-services tx provider update provider.yaml --offline --generate-only --from $PROVIDER_ADDRESS | jq -r '.body.messages[]' | jq -r 'del(."@type")' | sha1sum | awk '{print $1}')"
 if [[ "$REMOTE_PROVIDER" != "$LOCAL_PROVIDER" ]]; then
   echo "Updating provider in the blockchain ..."
-  /bin/akash tx provider update provider.yaml >deploy.log 2>&1
+  provider-services tx provider update provider.yaml >deploy.log 2>&1
   DEPLOY=$(cat deploy.log)
 
   if [[ $DEPLOY == *"insufficient fees"* ]]; then
@@ -113,14 +113,14 @@ fi
 CERT_PATH="${AKASH_HOME}/${PROVIDER_ADDRESS}.pem"
 # 0 = yes; otherwise do not (re-)generate new provider certificate, unless
 GEN_NEW_CERT=1
-LAST_CERT_STATUS="$(akash query cert list --owner $PROVIDER_ADDRESS --state valid -o json | jq -r '.certificates[-1].certificate.state')"
+LAST_CERT_STATUS="$(provider-services query cert list --owner $PROVIDER_ADDRESS --state valid -o json | jq -r '.certificates[-1].certificate.state')"
 # Check whether the last broadcasted certificate is valid
 if [[ "valid" != "$LAST_CERT_STATUS" ]]; then
   echo "No valid certificate found for provider: $PROVIDER_ADDRESS"
   GEN_NEW_CERT=0
 
   echo "It might as well be that the current certificate was expired, thus, it should be safe to delete it locally"
-  # It's also a good idea to delete it as otherwise, we'd have to add `--overwrite` to `akash tx cert generate server` command later.
+  # It's also a good idea to delete it as otherwise, we'd have to add `--overwrite` to `provider-services tx cert generate server` command later.
   if [[ -f "${CERT_PATH}" ]]; then
     rm -vf "${CERT_PATH}"
   fi
@@ -129,7 +129,7 @@ fi
 # Check whether the local certificate matches the one on the blockchain
 if [[ -f "${CERT_PATH}" ]]; then
   LOCAL_CERT_SN="$(cat "${CERT_PATH}" | openssl x509 -serial -noout)"
-  REMOTE_CERT_SN="$(AKASH_OUTPUT=json akash query cert list --owner $PROVIDER_ADDRESS --state valid | jq -r '.certificates[-1].certificate.cert' | openssl base64 -A -d | openssl x509 -serial -noout)"
+  REMOTE_CERT_SN="$(AKASH_OUTPUT=json provider-services query cert list --owner $PROVIDER_ADDRESS --state valid | jq -r '.certificates[-1].certificate.cert' | openssl base64 -A -d | openssl x509 -serial -noout)"
 else
   echo "${CERT_PATH} file is missing."
   GEN_NEW_CERT=0
@@ -147,7 +147,7 @@ elif [[ "$LOCAL_CERT_SN" != "$REMOTE_CERT_SN" ]]; then
 fi
 
 # generate a new cert if the current one expires sooner than 7 days
-AKASH_OUTPUT=json akash query cert list --owner $PROVIDER_ADDRESS --state valid | jq -r '.certificates[-1].certificate.cert' | openssl base64 -A -d | openssl x509 -checkend 604800 -noout 2>/dev/null 1>&2
+AKASH_OUTPUT=json provider-services query cert list --owner $PROVIDER_ADDRESS --state valid | jq -r '.certificates[-1].certificate.cert' | openssl base64 -A -d | openssl x509 -checkend 604800 -noout 2>/dev/null 1>&2
 rc=$?
 if [[ $rc -ne 0 ]]; then
   echo "Certificate expires in less than 7 days, so going to generate a new one."
@@ -156,9 +156,12 @@ fi
 
 if [[ "$GEN_NEW_CERT" -eq "0" ]]; then
   echo "Generating new provider certificate"
-  /bin/akash tx cert generate server provider.{{ .Values.domain }}
+  provider-services tx cert generate server provider.{{ .Values.domain }}
   echo "Publishing new provider certificate"
-  /bin/akash tx cert publish server
+  provider-services tx cert publish server
 fi
 
-exit 0
+##
+# Run daemon
+##
+provider-services run --cluster-k8s
