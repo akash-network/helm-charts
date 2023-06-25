91dc21f2154975be8cbc8d66bd8b814bb5b91af5
Andrey Arapov <107317698+andy108369@users.noreply.github.com>
2023-06-25 15:48:50 +0200

    fix(pricing script): awk is part of the mawk (#208)

--- charts/akash-provider/scripts/run.sh
+++ charts/akash-provider/scripts/run.sh
@@ -4,7 +4,7 @@
 exec &> >(tee -a "/var/log/provider.log")
 
 # Install apps required by the bid price script
-apt -qq update && DEBIAN_FRONTEND=noninteractive apt -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --no-install-recommends install curl jq bc awk ca-certificates
+apt -qq update && DEBIAN_FRONTEND=noninteractive apt -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --no-install-recommends install curl jq bc mawk ca-certificates
 
 # fail fast should there be a problem installing curl / jq packages
 type curl || exit 1
