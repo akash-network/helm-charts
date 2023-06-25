af085577652a261c99fd32e5de872f2f93959918
Andrey Arapov <107317698+andy108369@users.noreply.github.com>
2023-06-25 15:42:39 +0200

    fix: correct pricing calculations for multi-pod leases (akash-network/support#103) (#207)

--- charts/akash-provider/scripts/run.sh
+++ charts/akash-provider/scripts/run.sh
@@ -4,10 +4,12 @@
 exec &> >(tee -a "/var/log/provider.log")
 
 # Install apps required by the bid price script
-apt -qq update && DEBIAN_FRONTEND=noninteractive apt -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --no-install-recommends install curl jq bc ca-certificates
+apt -qq update && DEBIAN_FRONTEND=noninteractive apt -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --no-install-recommends install curl jq bc awk ca-certificates
 
 # fail fast should there be a problem installing curl / jq packages
 type curl || exit 1
 type jq || exit 1
+type awk || exit 1
+type bc || exit 1
 
 exec provider-services run
