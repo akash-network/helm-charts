8dc249228135c60a8bc1634c3db0c830bdeceeee
Zeke <105066639+Zblocker64@users.noreply.github.com>
2025-01-28 21:44:21 -0600

    Revert "feat: update provider to v0.6.5" (#304)
    Reverts akash-network/helm-charts#303

--- charts/akash-provider/scripts/run.sh
+++ charts/akash-provider/scripts/run.sh
@@ -19,5 +19,11 @@ type bc || exit 1
 ##
 /scripts/refresh_provider_cert.sh
 
-# Start provider-services
-exec provider-services run
+# Start provider-services and monitor its output
+exec provider-services run | while read line; do
+    echo "$line"
+    if [[ "$line" == *"account sequence mismatch"* ]]; then
+        echo "Pattern 'account sequence mismatch' found. Restarting provider-services..."
+        exit 2
+    fi
+done
