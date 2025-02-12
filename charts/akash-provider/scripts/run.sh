0ee9484281e90ea5a2473e43f7449f09c8443be0
Andrey Arapov <107317698+andy108369@users.noreply.github.com>
2025-02-12 16:01:00 +0100

    fix(provider): remove package deps (#328)
    as they will be delivered by the image now

--- charts/akash-provider/scripts/run.sh
+++ charts/akash-provider/scripts/run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Install apps required by the bid price script
-apt -qq update && DEBIAN_FRONTEND=noninteractive apt -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --no-install-recommends install curl jq bc mawk ca-certificates
-
-# fail fast should there be a problem installing curl / jq packages
+# fail fast should these packages be missing
 type curl || exit 1
 type jq || exit 1
 type awk || exit 1
